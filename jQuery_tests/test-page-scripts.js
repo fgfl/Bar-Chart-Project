23cbb6386fec8c534590c1305ff1a68fb374e9dc
Frederick <frederick.gfl@gmail.com>
2019-11-07 06:45:07 +0000

    separate script from html

---
+++ jQuery_tests/test-page-scripts.js
@@ -0,0 +1,18 @@
+
+
+$( document ).ready(function() {
+    $( "a" ).click(function( event ) {
+        event.preventDefault();
+        $(this).hide("slow");
+    });
+
+
+    $("#addButton").click(function( event) {
+      $("a").addClass("test")
+      $("a").show();
+    })
+
+    $("#removeButton").click(function( event) {
+      $("a").removeClass("test")
+    })
+});
